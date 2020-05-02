31acc2c463b5719e132647ccd36e24d70fed1c59
MrSamwell <samuelbs98@gmail.com>
2020-05-02 18:10:56 -0300

    Bunch

---
+++ main.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: '#app',
+    data: {
+        apiData: false,
+        deaths: 0,
+        cases: 0,
+        recovered: 0
+    },
+    methods: {
+        getApiData: function() {
+            fetch(`https://api.covid19api.com/summary`)
+            .then(response => response.json())
+            .then(data => { this.apiData = data; 
+                })
+            
+            .catch(err => alert("Woops! Something went wrong."))
+        },
+        getNumbers: function(){
+            this.getApiData();
+            alert(this.apiData)
+
+        }
+    },
+    mounted: function(){
+        this.getNumbers();
+        
+    }
+    
+})
