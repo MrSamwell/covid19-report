6addbf51dabb8347df522e7c47b79ceae84ffc8a
MrSamwell <samuelbs98@gmail.com>
2020-05-02 18:51:19 -0300

    Almost Final Finally

--- main.js
+++ main.js
@@ -17,7 +17,17 @@ new Vue({
             .catch(err => alert("Woops! Something went wrong."))
         },
         getNumbers: function(){
+            if(this.$refs.radioMundo.checked){
             console.log(this.apiData); 
+            this.deaths = this.apiData.Global.TotalDeaths;
+            this.cases = this.apiData.Global.TotalConfirmed;
+            this.recovered = this.apiData.Global.TotalRecovered;
+            }
+            else{
+                this.deaths = this.apiData.Countries[29].TotalDeaths;
+                this.cases = this.apiData.Countries[29].TotalConfirmed;
+                this.recovered = this.apiData.Countries[29].TotalRecovered;
+            }
         }
     },
     mounted: function(){
